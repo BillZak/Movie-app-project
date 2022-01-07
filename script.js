64b4b3bf513e3e60352cfea7aaeb68afc492854f
Bill Zack <bzak954@gmail.com>
2022-01-07 17:50:08 +0000

    add api key

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=61d86a3a66ae8b3e4f760720698f5e36&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=61d86a3a66ae8b3e4f760720698f5e36&query="'
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c4971420a1bf9a5e0735e13bc23c7fb5&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=c4971420a1bf9a5e0735e13bc23c7fb5&query="';
 
-const main = document.getElementById('main')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-getMovies(API_URL)
+getMovies(API_URL);
 
-async function getMovies(url){
-    const res = await fetch(url)
-    const data = await res.json()
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
 
-    showMovies(data.results)
+  showMovies(data.results);
 }
 
-function showMovies(movies){
-    main.innerHTML = ''  
+function showMovies(movies) {
+  main.innerHTML = "";
 
-    movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview} = movie
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
 
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
 
-        movieEl.innerHTML =`
+    movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
         
             <div class="movie-info">
@@ -35,32 +37,32 @@ function showMovies(movies){
             <h3>Overview</h3>
            ${overview}
             </div>   
-        `
+        `;
 
-        main.appendChild(movieEl)
-    })
+    main.appendChild(movieEl);
+  });
 }
 
-function getClassByRate(vote){
-    if(vote >= 8){
-        return 'green'
-    }else if(vote >= 5){
-        return 'orange'
-    }else{
-        return 'red'
-    }
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const searchTerm = search.value
+  const searchTerm = search.value;
 
-    if(searchTerm && searchTerm !== ''){
-        getMovies(SEARCH_API + searchTerm)
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
 
-        search.value = ''
-    }else{
-        window.location.reload()
-    }
-})
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
